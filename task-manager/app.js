3e73835c89c11115a9fe8e846bdd7c737f131bfe
krewsona <ajkrewson@gmail.com>
2025-04-28 16:18:11 -0500

    practice

--- task-manager/app.js
+++ task-manager/app.js
@@ -15,28 +15,3 @@ listTasks(tasks);
 
 saveTasks(filePath, tasks);
 
-
-
-
-
-
-
-
-
-
-
-
-// const myTasks = [];
-
-// taskManager.addTasks(myTasks, 'do assignment');
-// taskManager.addTasks(myTasks, 'practice problems');
-
-// console.log(myTasks);
-
-// //file handler imported module
-// const jsonTasks = ['do assignemnt', 'practice problems'];
-
-// fileHandler.saveTasks('tasks.json', jsonTasks);
-
-// const loadedTasks = fileHandler.loadTasks('tasks.json');
-// console.log('Loaded tasks:', loadedTasks);
